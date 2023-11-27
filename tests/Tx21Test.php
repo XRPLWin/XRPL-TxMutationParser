2bf758a58e6875cece2ba25025b35ff66a2c46bc
ZGrguric <zgrguric@xrplwin.com>
2023-11-27 08:44:19 +0100

    Regular key not signer test

---
+++ tests/Tx21Test.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace XRPLWin\XRPLTxMutatationParser\Tests;
+
+use PHPUnit\Framework\TestCase;
+use XRPLWin\XRPLTxMutatationParser\TxMutationParser;
+
+/***
+ * PaymentChannelCreate
+ * Initiator has regular key but this transaction was signed with initiator account.
+ */
+final class Tx21Test extends TestCase
+{
+    public function testPaymentChannelCreateAccountRootRegularkeyNotSigner()
+    {
+        $transaction = file_get_contents(__DIR__.'/fixtures/tx21.json');
+        $transaction = \json_decode($transaction);
+        $account = "ravr52zHtsL6JZrWxz4aZe96rffg1ixwGT"; //account root regular key
+        $TxMutationParser = new TxMutationParser($account, $transaction->result);
+        $parsedTransaction = $TxMutationParser->result();
+        //dd($parsedTransaction);
+        //Self (own account) must be $account
+        $this->assertEquals($account,$parsedTransaction['self']['account']);
+
+        # Basic info
+
+        //Own account: one balance change
+        $this->assertEquals(0,count($parsedTransaction['self']['balanceChanges']));
+
+        //Transaction type SENT
+        $this->assertEquals(TxMutationParser::MUTATIONTYPE_UNKNOWN,$parsedTransaction['type']);
+    }
+}
